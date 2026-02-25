e8b121b6c107b75b59ad7524d887a2ee1aaee1bf
Mikhail Khrunov <m.hrunov@yandex.ru>
2026-02-25 21:49:51 +0400

    trying to fix repos

--- fastapi_app/src/schemas/cats.py
+++ fastapi_app/src/schemas/cats.py
@@ -4,16 +4,20 @@ from typing import Literal, List, Optional
 MyColors = Literal["Gray", "Black", "White", "Ginger", "Mixed"]
 
 class BaseCat(BaseModel):
-    name: str = Field(..., min_length=1, max_legnth=16)
+    name: str = Field(..., min_length=1, max_length=16)
     color: MyColors
-    birth_year: int = Field(..., gr=1900, le=2100) # ge - >=, le - <=
+    birth_year: int = Field(..., ge=1900, le=2100) # ge - >=, le - <=
 
 class CatCreate(BaseCat):
     owner_id: int
     achievements: Optional[List[str]] = Field(default_factory=list)
 
 class CatUpdate(BaseModel):
+<<<<<<< HEAD
     name: str = Field(..., min_length=1, max_length=16)
+=======
+    name: Optional[str] = Field(None, min_length=1, max_length=16)
+>>>>>>> 4d5effb8c1f7e89e3f7792b18aafa73f256e8139
     color: Optional[MyColors] = None
     birth_year: Optional[int] = Field(None, ge=1900, le=2100)
     achievements: Optional[List[str]] = None
